9590f664dcdb005f553027f815afb11bcd0f14f8
EwenBriand <ewen.briand@epitech.eu>
2023-02-22 17:04:03 +0100

    [ADD] last hope

--- mystrncmp.asm
+++ mystrncmp.asm
@@ -1,37 +1,84 @@
 
 SECTION .text
-    global strncmp
+    GLOBAL mystrncmp
 
-strncmp:
+mystrncmp:
     push rbp
-    mov rbp, rsp
     push rdi
     push rsi
+    push rcx
     push rdx
-    push r10
-    push r8
-    xor r10, r10
-    xor r8, r8
+    mov rbp, rsp
+    xor rcx, rcx
+    mov rax, 0
+    jmp ncmploop
 
-mloop:
-    cmp rdx, 0
-    jle end
-    mov r10b, byte [rdi]
-    mov r8b, byte [rsi]
-    cmp r10b, r8b
+ncmploop:
+    cmp rcx, rdx
+    jge end
+    mov al, [rdi + rcx]
+    mov bl, [rsi + rcx]
+    cmp al, bl
     jne end
-    inc rsi
-    inc rdi
-    dec rdx
-    jmp mloop
+    cmp al, 0
+    je end
+    inc rcx
+    jmp ncmploop
 
-end:
-    sub r10b, r8b
-    movsx rax, r10b
-    pop r8
-    pop r10
+end2:
+    xor rax, rax
     pop rdx
+    pop rcx
     pop rsi
     pop rdi
     pop rbp
     ret
+
+end:
+    sub al, bl
+    movsx rax, al
+    cmp rdx, 0
+    je end2
+    pop rdx
+    pop rcx
+    pop rsi
+    pop rdi
+    pop rbp
+    ret
+
+; SECTION .text
+;     global strncmp
+
+; strncmp:
+;     push rbp
+;     mov rbp, rsp
+;     push rdi
+;     push rsi
+;     push rdx
+;     push r10
+;     push r8
+;     xor r10, r10
+;     xor r8, r8
+
+; mloop:
+;     cmp rdx, 0
+;     jle end
+;     mov r10b, byte [rdi]
+;     mov r8b, byte [rsi]
+;     cmp r10b, r8b
+;     jne end
+;     inc rsi
+;     inc rdi
+;     dec rdx
+;     jmp mloop
+
+; end:
+;     sub r10b, r8b
+;     movsx rax, r10b
+;     pop r8
+;     pop r10
+;     pop rdx
+;     pop rsi
+;     pop rdi
+;     pop rbp
+;     ret
--- mystrrchr.asm
+++ mystrrchr.asm
@@ -1,5 +1,29 @@
-section    .text
-   [global strrchr:]
+SECTION .text
+   GLOBAL mystrrchr
+
+
+mystrrchr:
+    push rbp
+    push rdi
+    push rsi
+    mov rbp, rsp
+    xor rax, rax
+    jmp rchrloop
+
+
+change_rax:
+    mov rax, rdi
+    jmp after_change
+
+rchrloop:
+    cmp sil, byte [rdi]
+    je change_rax
+
+after_change:
+    cmp byte [rdi], 0
+    je strrchr_end
+    inc rdi
+    jmp rchrloop
 
 strrchr_end:
     pop rsi
@@ -7,23 +31,34 @@ strrchr_end:
     pop rbp
     ret
 
-change_rax:
-    mov rax, rdi
-    jmp after_change
 
-loop:
-    cmp sil, byte [rdi]
-    je change_rax
-after_change:
-    cmp byte [rdi], 0
-    je strrchr_end
-    inc rdi
-    jmp loop
 
-strrchr:
-    push rbp
-    push rdi
-    push rsi
-    mov rbp, rsp
-    xor rax, rax
-    jmp loop
+
+; SECTION .text
+;   GLOBAL strrchr
+
+; strrchr:
+;     push rbp
+;     push rdi
+;     push rsi
+;     push r10
+;     mov rbp, rsp
+;     mov r10, -1
+;     mov rax , 0
+
+; chrrloop:
+;     inc  r10
+;     cmp byte [rdi + r10] , sil
+;     je chrrloop_true
+;     cmp byte [rdi + r10] , 0
+;     jne chrrloop
+
+;     pop r10
+;     pop rsi
+;     pop rdi
+;     pop rbp
+;     ret
+
+; chrrloop_true:
+;   lea rax, [rdi + r10]
+;   jmp chrrloop
