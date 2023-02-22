7e2a38009fd00bfe372abd2c8f3cec3e91ec8afc
EwenBriand <ewen.briand@epitech.eu>
2023-02-22 16:50:11 +0100

    [ADD] hello world

--- mystrrchr.asm
+++ mystrrchr.asm
@@ -1,28 +1,29 @@
-SECTION .text
-  GLOBAL strrchr
+section    .text
+   [global strrchr:]
 
-strrchr:
-    push rbp
-    push rdi
-    push rsi
-    push r10
-    mov rbp, rsp
-    mov r10, -1
-    mov rax , 0
-
-chrrloop:
-    inc  r10
-    cmp byte [rdi + r10] , sil
-    je chrrloop_true
-    cmp byte [rdi + r10] , 0
-    jne chrrloop
-
-    pop r10
+strrchr_end:
     pop rsi
     pop rdi
     pop rbp
     ret
 
-chrrloop_true:
-  lea rax, [rdi + r10]
-  jmp chrrloop
+change_rax:
+    mov rax, rdi
+    jmp after_change
+
+loop:
+    cmp sil, byte [rdi]
+    je change_rax
+after_change:
+    cmp byte [rdi], 0
+    je strrchr_end
+    inc rdi
+    jmp loop
+
+strrchr:
+    push rbp
+    push rdi
+    push rsi
+    mov rbp, rsp
+    xor rax, rax
+    jmp loop
